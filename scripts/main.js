49be859b05b128db5ed94374f79b6e3992bf7e53
shoe-leps <shurjo@letutorial.com>
2016-08-14 02:18:33 +0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var Hello = React.createClass({
+    render: function() {
+        var text = "this is the variable text"
+        return(
+            <div>{text}</div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("reactstuff")
+);
